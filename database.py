3198b5e0ff80b96752c49216734d08204cf07c05
Erfan Dousti <erfan.flashpoint@gmail.com>
2026-04-26 15:58:46 +0330

    First table with sqlalchemy, gotta say it is weired

---
+++ database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+
+SQLALCHEMY_DB_URL = "sqlite:///todos.db"
+
+engine = create_engine(SQLALCHEMY_DB_URL, connect_args={"check_same_thread":False})
+
+SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
+Base = declarative_base()
+
+
+
