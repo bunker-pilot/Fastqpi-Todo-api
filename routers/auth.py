699423c7c95328dc7e52b54b405e19dacbb8b7c8
Erfan Dousti <erfan.flashpoint@gmail.com>
2026-04-27 18:37:59 +0330

    Added jwt and get token endpoint

--- routers/auth.py
+++ routers/auth.py
@@ -1,11 +1,20 @@
-from fastapi import  APIRouter,HTTPException
+from datetime import datetime, timedelta, timezone
+from fastapi import  APIRouter,HTTPException, Depends
+from fastapi.security import OAuth2PasswordRequestForm
 from pydantic import BaseModel, EmailStr, Field, field_validator
 from passlib.context import CryptContext
 from starlette import status
+from jose import jwt
+from typing import Annotated
 import models
 from database import db_dependency
 
 router = APIRouter()
+SECRET_KEY= "dGhpcy1pcy1hLXNlY3JldC1rZXktZm9yLWp3dC1zdGF5LXNhZmUtaXQtaXMtbG9uZy1lbm91Z2gK"
+ALGORITHM = "HS256"
+
+
+
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -37,6 +46,10 @@ class UserResponse(BaseModel):
     class Config:
         from_attributes = True
 
+class Token(BaseModel):
+    access_token : str
+    token_type: str
+
 
 
 
@@ -47,7 +60,18 @@ def verify_password(password:str, hashed_pass:str) -> bool:
     return pwd_context.verify(password, hashed_pass)
 
 
+def authenticate_user(username:str, password:str, db):
+    user = db.query(models.User).filter((models.User.username == username)).first()
+    if user is not None: 
+        if verify_password(password, user.hashed_password):
+            return user
+    return None
 
+def create_access_token(username: str, user_id:int,  expires_delta: timedelta):
+    encode = {"sub" : username, "id": user_id}
+    expires = datetime.now(timezone.utc) + expires_delta
+    encode.update({"exp": expires})
+    return jwt.encode(encode, SECRET_KEY, ALGORITHM)
 
 
 
@@ -75,3 +99,11 @@ def register_user(db: db_dependency, data:UserRequest):
     return new_user
     
 
+@router.post("/api/token/", status_code=status.HTTP_200_OK, response_model=Token)
+def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+                           db: db_dependency):
+    user = authenticate_user(form_data.username, form_data.password , db)
+    if user is not None :
+        token = create_access_token(user.username, user.id,timedelta(minutes=20) )
+        return {"access_token": token, "token_type": "bearer"}
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="wrong username or password ")
